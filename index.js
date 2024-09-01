eb2ddf0689618ae7fbf991458f699d998f5b8347
Vivek Shankar Phadake <129990463+Vivek20sp@users.noreply.github.com>
2024-09-01 22:23:16 +0530

    Sixth Commit

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const app = express();
 const port = process.env.PORT;
 ConnectToDB();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000', // Allow requests from this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+    credentials: true, // If your requests include credentials like cookies
+}));
 
 app.get('/',(req,res)=>{
     res.send('Hello World');
