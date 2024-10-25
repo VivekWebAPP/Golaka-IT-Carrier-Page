ec65eb05debbfa80bf941bfcba88871306fa57a0
Vivek Shankar Phadake <129990463+Vivek20sp@users.noreply.github.com>
2024-10-25 22:21:34 +0530

    Third Commit

--- index.js
+++ index.js
@@ -5,15 +5,29 @@ import userRoutes from './routes/userRegestration.js';
 import adminRoutes from './routes/adminAuth.js';
 import jobRoutes from './routes/JobCreationRoute.js';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5002;
+const assiginedOrigin = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:3002',
+  'http://localhost:3003',
+  'http://localhost:3004',
+  'http://localhost:3005',
+]
 
 app.use(express.json());  // Parse JSON bodies
 // app.use(bodyParser.urlencoded({ extended: true }))
 // app.use(express.urlencoded({ extended: false }));
+app.use(cors({
+  origin: assiginedOrigin, // Allow requests from this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+  credentials: true, // If your requests include credentials like cookies
+}));
 
 createTables();  // Ensure tables are created
 
